8a391a47e47f83ae6235ac2bacd18481042da12c
gruler05 <bansodegaurav@gmail.com>
2019-01-24 22:34:00 -0600

    Fixing the build and the mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -6,18 +6,24 @@ const Mutation = {
         //  hash the password
         const password = await bcrypt.hash(args.password, 10);
         const user = await ctx.db.mutation.createUser({
-            ...args,
-            password,
-            permissions: { set: ['USER'] },
+            data: {
+                ...args,
+                password,
+                permissions: {
+                    set: ['USER']
+                },
+            }
         }, info);
         //  info as a second argument to know what data to be returned to the client
 
         //  create a JWT token for them
-        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        const token = jwt.sign({
+            userId: user.id
+        }, process.env.APP_SECRET);
         // We set a jwt as a cookie on the response
         ctx.response.cookie('token', token, {
             httpOnly: true,
-            maxAge: 1000 * 60 * 60 *24 *365, // 1 year cookie
+            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
         });
 
         return user;
